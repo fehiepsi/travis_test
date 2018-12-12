87993cc24714723f4c0c8d9e7fc30fc12b9ed6a9
fehiepsi <fehiepsi@gmail.com>
2018-12-13 03:20:44 +0900

    abc

--- conv2d.py
+++ conv2d.py
@@ -7,6 +7,6 @@ x = torch.randn(1000, 1, 28, 28)
 conv = torch.nn.Conv2d(1, 10, kernel_size=5)
 start = time.time()
 o = conv(x)
-print(time.time() - start)
-print(o.sum().item())
+print("Time spent for conv:", time.time() - start)
+print("Sum of output:", o.sum().item())
 
